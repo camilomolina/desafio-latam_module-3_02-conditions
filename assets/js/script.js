bf8caeca5db308b7d730c31e83f60ef1329091dd
Camilo Molina Orth <camilo.molina.orth@gmail.com>
2025-10-22 23:41:31 -0300

    wip: challenge 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 // valores iniciales
+let borderStatus = false;
+
+let imgJS = document.getElementById("img-js");
+
+imgJS.addEventListener("click", () => {
+  borderStatus = !borderStatus;
+
+  if (borderStatus) {
+    imgJS.style.border = "2px solid rgba(255, 0, 0, 1)";
+  } else {
+    imgJS.style.border = "none";
+  }
+});
